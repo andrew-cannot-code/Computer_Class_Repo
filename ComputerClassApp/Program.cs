631a33ed8a04bb421d2da3a02319ea979a943059
andrew-cannot-code <fam4h02@gmail.com>
2025-10-03 11:53:01 -0500

    First code!

--- ComputerClassApp/Program.cs
+++ ComputerClassApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// My name is Andrew Hoorelbek. I originated this code.
+// Input name and what you did here (like: My name is John and I edited the code first)
+// Input name and what you did here (like: My name is John and I edited the code first)
+// Input name and what you did here (like: My name is John and I edited the code first)
+// Input name and what you did here (like: My name is John and I edited the code first)
+// DO NOT delete this line!!! 
+
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("What shall we do?");
+byte math = 1 + 1;
+Console.WriteLine($"1 + 1 = " + math);
+Console.WriteLine("Goodbye, World!");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+// Try the program! Go ahead and run it. You should see the output in the console window.
+// Now, make a change to the code. Change the math variable to do a different calculation
