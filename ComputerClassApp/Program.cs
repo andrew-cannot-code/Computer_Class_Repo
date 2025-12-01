b3603aea9560bd79e9548f4ca6839475ebb97a15
andrew-cannot-code <fam4h02@gmail.com>
2025-12-01 11:28:07 -0600

    Added some user input... Brain's fried now :). ~Andrew

--- ComputerClassApp/Program.cs
+++ ComputerClassApp/Program.cs
@@ -8,11 +8,21 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("What shall we do?");
-byte math = 1 + 1;
-Console.WriteLine($"1 + 1 = " + math);
+Console.WriteLine("Do you want to see some math?"); // From here...
+// Get user input
+if (Console.ReadLine()?.ToLower() == "yes")
+{
+    Console.WriteLine("Great! Let's do some math.");
+    byte math = 100 % 6;
+    Console.WriteLine($"100 % 6 = {math}");
+}
+else
+{
+    Console.WriteLine("No problem! Maybe next time.");
+}                                                      // ...to here, I, Andrew, added a simple math operation based on user input.
+
 Console.WriteLine("Goodbye, World!");
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
 // Try the program! Go ahead and run it. You should see the output in the console window.
 // Now, make a change to the code. Change the math variable to do a different calculation
-
